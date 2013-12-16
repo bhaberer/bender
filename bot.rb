6496712e6ed99f540ccbd1b2e5e0a114dbb2155f
Brian Haberer <bhaberer@gmail.com>
2013-12-16 14:59:56 -0800

    Small cleanup

--- bot.rb
+++ bot.rb
@@ -21,7 +21,7 @@ require 'cinch-urbandict'
 require 'cinch-wikipedia'
 
 # Load the bot config
-conf = Psych::load(File.open('config/bot.yml'))
+conf = Psych.load(File.open('config/bot.yml'))
 
 # Init Bot
 @bot = Cinch::Bot.new do
@@ -35,12 +35,13 @@ conf = Psych::load(File.open('config/bot.yml'))
 
     # Plugins
     c.plugins.prefix  = '.'
-    c.plugins.plugins = Cinch::Plugins.constants.map { |c| Class.module_eval("Cinch::Plugins::#{c}") }
+    c.plugins.plugins =
+      Cinch::Plugins.constants.map do |plugin|
+        Class.module_eval("Cinch::Plugins::#{plugin}")
+      end
 
     # Setup the cooldown if one is configured
-    if conf.key?(:cooldowns)
-      c.shared[:cooldown] = { :config => conf[:cooldowns] }
-    end
+    c.shared[:cooldown] = { config: conf[:cooldowns] } if conf.key?(:cooldowns)
 
     # Link logger config
     if conf.key?(:links) && defined?(Cinch::Plugins::LinksLogger)
@@ -59,22 +60,25 @@ conf = Psych::load(File.open('config/bot.yml'))
   end
 
   on :channel, /\A\.stats\z/ do |m|
-    if conf[:stats_url]
-      m.user.send "The stats for the channel are available at: #{conf[:stats_url]}"
+    if conf.key?(:stats_url)
+      m.user.send 'The stats for the channel are available at: ' +
+                  conf[:stats_url]
     else
-      m.user.send "No stats page has been defined for this channel, sorry!"
+      m.user.send 'No stats page has been defined for this channel, sorry!'
     end
   end
 
   on :channel, /\A\.help\z/ do |m|
-    m.user.send "Hello, my name is #{conf[:nick]}, and I'm the #{m.channel.name} bot."
-    m.user.send "You can find out more about me and how to file feature requests / bugs by visiting #{conf[:wiki_url]}"
+    m.user.send 'Hello, my name is #{conf[:nick]}, and I\'m ' +
+                "the #{m.channel.name} bot."
+    if conf.key?(:wiki_url)
+      m.user.send 'You can find out more about me and how to file feature' +
+                  "requests / bugs by visiting #{conf[:wiki_url]}"
+    end
   end
 
   on :notice, /IDENTIFY/ do |m|
-    if m.user.nick == 'NickServ'
-      m.reply "IDENTIFY #{conf[:nickserv_pass]}"
-    end
+    m.reply "IDENTIFY #{conf[:nickserv_pass]}" if m.user.nick == 'NickServ'
   end
 end
 
