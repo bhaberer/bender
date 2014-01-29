106f1b8f6e53b32524481680e35af4095c25f3cb
Brian Haberer <bhaberer@gmail.com>
2014-01-28 23:56:27 -0800

    updates!

--- dbstatspage.rb
+++ dbstatspage.rb
@@ -1,9 +1,10 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 require 'rubygems'
+require 'psych'
 require 'bundler/setup'
-require 'yaml'
 require 'cinch-bag'
+require 'cinch-hangouts'
 
 # GROSS AS HELL; but whatever, it's irc shit.
 
@@ -28,6 +29,7 @@ def html_head
                 <li><a href='/'>Channel Stats</a></li>
                 <li><a class='active'}' href='dickbag.html'>Dickbag</a></li>
                 <li><a class='active'}' href='karma.html'>Karma</a></li>
+                <li><a class='active'}' href='hangouts.html'>Hangouts</a></li>
                 <li><a href='http://shitenforcerslink.tumblr.com/'>Link Logger</a></li>
               </ul>
             </div>
@@ -75,9 +77,11 @@ end
 @db_htmlpage = '/home/bhaberer/src/pisg/estats/dickbag.html'
 @karma_filename = '/home/bhaberer/src/bender/yaml/karma.yml'
 @karma_htmlpage = '/home/bhaberer/src/pisg/estats/karma.html'
+@hangouts_filename = '/home/bhaberer/src/bender/yaml/hangouts.yml'
+@hangouts_htmlpage = '/home/bhaberer/src/pisg/estats/hangouts.html'
 
 # Get Dickbag stats
-@data = YAML::load(File.open(@db_filename))
+@data = Psych.load(File.open(@db_filename))
 @stats = []
 
 @data[:stats].each_pair do |user, data|
@@ -91,8 +95,8 @@ end
 @stats.sort! {|x,y| y[:time] <=> x[:time] }
 @toptimes = @stats[0..49]
 
-# Get Karama stats
-@data = YAML::load(File.open(@karma_filename))
+# Get Karma stats
+@data = Psych.load(File.open(@karma_filename))
 @karma = []
 
 @data['#enforcers'].each_pair do |item, count|
@@ -100,11 +104,59 @@ end
 end
 @karma.sort! {|x,y| y[:count] <=> x[:count] }
 #@karma = @karma[0..199]
-@karma.delete_if { |k| k[:count].zero? } 
+@karma.delete_if { |k| k[:count].zero? }
+
+# Get Hangouts
+@data = Psych.load(File.open(@hangouts_filename))
+@hangouts = []
+
+@data[:hangouts].each_pair do |item, hangout|
+  @hangouts << { nick:  hangout.nick,
+                 id:    hangout.id,
+                 time:  hangout.time }
+end
 
 # Build Pages
 
+# hangouts
+@hangouts_html_out = html_head
+@hangouts_html_out << "
+            <br>
+            <div class=\"row\">
+              <div class=\"span6 offset2\">
+                <table class=\"table table-condensed table-bordered table-striped\">
+                  <thead>
+                    <th>Started By</th>
+                    <th>Time</th>
+                    <th>Link</th>
+                  </thead>"
+@hangouts.each do |hangout|
+  @hangouts_html_out << "<tr><td>"
+  @hangouts_html_out << hangout[:nick]
+  @hangouts_html_out << "</td><td>"
+  @hangouts_html_out << hangout[:time].strftime("%H:%M")
+  @hangouts_html_out << "</td><td>"
+  @hangouts_html_out << "<a href='#{Hangout.url(hangout[:id])}'>Click to Join</a>"
+  @hangouts_html_out << "</td></tr>"
+end
+
+@hangouts_html_out << "
+                </table>
+              </div>
+            </div>
+            <div class=\"row\">
+              <div class=\"span6 offset2\">
+                <div class=\"well text-center\">
+                  Hangout List Updated at #{Time.now.strftime("%H:%M on %Y-%m-%d")}
+                </div>
+              </div>
+            </div>"
+@hangouts_html_out << html_foot
+
+
+
 # dickbag
+
 @db_html_out = html_head
 @db_html_out << "
             <br>
@@ -188,4 +240,7 @@ File.open(@db_htmlpage, 'w') do |file|
 end
 File.open(@karma_htmlpage, 'w') do |file|
   file.puts @karma_html_out
+end
+File.open(@hangouts_htmlpage, 'w') do |file|
+  file.puts @hangouts_html_out
 end
